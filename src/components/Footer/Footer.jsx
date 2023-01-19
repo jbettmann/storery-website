17c316780ea3c38aa8fc89c45c1dfa602d9af050
jbettmann <jordan.bettmann@gmail.com>
2023-01-19 15:27:52 -0700

    created footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import sanityClient from "../../client";
+import { urlFor } from "../../Functions/Functions";
 
-export const Footer = () => {
-  return <div>Footer</div>;
+export const Footer = ({ logo }) => {
+  const [footer, setFooter] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == 'footer']{
+            title,
+            socialLinks,
+            name,
+            phone,
+            email,
+         }`
+      )
+
+      .then((data) => {
+        let [newData] = data;
+        setFooter(newData);
+      })
+      .catch(console.error);
+  }, []);
+  console.log(logo);
+  return (
+    footer && (
+      <div>
+        <img src={urlFor(logo.asset._ref)} alt="Storey Real Estate Logo" />
+      </div>
+    )
+  );
 };

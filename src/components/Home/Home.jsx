6f28d9aa027852ef2fb723ecba38454b8441a597
jbettmann <jordan.bettmann@gmail.com>
2023-01-18 21:24:21 -0700

    imported video player for Youtube

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,5 +5,33 @@ import { MyVideo } from "../MyVideo/MyVideo";
 import { Footer } from "../Footer/Footer";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [home, setHome] = useState(null);
+  const [aboutVid, setAboutVid] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == 'home']{
+          title,
+          slug,
+          hero,
+          cards,
+          homeAbout,
+      }`
+      )
+
+      .then((data) => {
+        let [newData] = data;
+        setHome(newData);
+        setAboutVid(newData.homeAbout.videoUrl);
+      })
+      .catch(console.error);
+  }, [home?.homeAbout.videoUrl]);
+  console.log(aboutVid);
+  if (!home) return <div>Loading...</div>;
+  return (
+    <div>
+      <MyVideo url={aboutVid} />
+    </div>
+  );
 };

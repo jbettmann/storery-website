1cdb51488637adaa0d99dc5cd660bd600ad48312
jbettmann <jordan.bettmann@gmail.com>
2023-01-18 21:23:54 -0700

    created Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import sanityClient from "../../client";
+import { MyVideo } from "../MyVideo/MyVideo";
+import { Footer } from "../Footer/Footer";
 
 export const Home = () => {
   return <div>Home</div>;

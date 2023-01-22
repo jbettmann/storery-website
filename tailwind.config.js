1ac8d536ab6f56cb98d0eb8b5b75c86ee5293efd
jbettmann <jordan.bettmann@gmail.com>
2023-01-22 15:15:16 -0700

    added customization to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    // screens: {
+    //   sm: "720px",
+    //   md: "850px",
+    //   lg: "1200px",
+    // },
+    // spacing: {
+    //   1: "8px",
+    //   2: "12px",
+    //   3: "16px",
+    //   4: "24px",
+    //   5: "32px",
+    //   6: "48px",
+    // },
+    extend: {
+      colors: {
+        storeyGreen: {
+          100: "#094d26",
+          200: "#09f923",
+        },
+      },
+    },
   },
   plugins: [],
 };

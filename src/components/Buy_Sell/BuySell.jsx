2925dd6b24b79865b981c7056395fa6e58629db2
jbettmann <jordan.bettmann@gmail.com>
2023-01-23 14:56:41 -0700

    messed around with effects and animations

--- src/components/Buy_Sell/BuySell.jsx
+++ src/components/Buy_Sell/BuySell.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const BuySell = () => {
-  return <div>BuySell</div>;
+  return (
+    <div className="border rounded-md w-64 mx-auto text-center border-slate-200/50 p-4 shadow-2xl shadow-teal-700/50">
+      <h1 className="text-3xl m-4">Title</h1>
+      <button className="rounded-xl w-auto text-center text-white p-3 focus:ring-offset-2 bg-teal-400 shadow-lg shadow-teal-600/50 transition-all ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
+        BuySell
+      </button>
+    </div>
+  );
 };

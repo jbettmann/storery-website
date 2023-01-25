08ed8283f759a00be4c07e35786f6f0811c59e01
jbettmann <jordan.bettmann@gmail.com>
2023-01-25 16:50:44 -0700

    continuted design of hero, about and cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import sanityClient from "../../client";
+import { urlFor } from "../../Functions/Functions";
+
 import { MyVideo } from "../MyVideo/MyVideo";
 
 import "./Home.scss";
@@ -24,7 +26,8 @@ export const Home = ({ setLogo, contact }) => {
           hero,
           cards,
           homeAbout,
-          logo
+          logo,
+          language
       }`
       )
 
@@ -37,34 +40,75 @@ export const Home = ({ setLogo, contact }) => {
       })
       .catch(console.error);
   }, [home?.homeAbout.videoUrl]);
+  console.log(home);
   if (!home) return <div>Loading...</div>;
   return (
     <div>
       {/* Hero */}
-      <header>
-        <img src="" alt="" />
-        <h1></h1>
-        <h5></h5>
-        <NavLink to={`/${contact}`} aria-label="Request a Call link">
-          Request A Call
-        </NavLink>
+      <header className="my-8 relative text-white">
+        <div className=" h-64 md:h-96 lg:h-[30rem] overflow-hidden object-center ">
+          <img
+            src={urlFor(home.hero?.mainImage.asset._ref)}
+            alt="Beautiful sunny brick home surrounded by green trees that Storey owns"
+            className="mx-auto -translate-y-1/4 md:-translate-y-1/4 lg:-translate-y-1/3 2xl:-translate-y-1/2 w-full brightness-50 "
+          />
+        </div>
+        <article className="absolute top-0 left-0 w-full h-full px-6 flex flex-col text-center justify-center items-center ">
+          {home.hero.title.split("\n").map((t) => (
+            <h1 className="mb-0">{t}</h1>
+          ))}
+          <h5>{home.hero.body}</h5>
+          <NavLink
+            className="btn mt-8"
+            to={`/${contact}`}
+            aria-label="Request a Call link"
+          >
+            Request A Call
+          </NavLink>
+        </article>
       </header>
 
       {/* Cards */}
       {cards && (
-        <section>
+        <section className="flex gap-20 mx-9 justify-center">
           {cards.map((card, i) => {
             return (
-              <div key={i}>
-                <img src={card?.image} alt="" />
+              <div
+                key={i}
+                className=" bg-white text-center max-w-2xl h-auto p-10"
+              >
+                <img
+                  src={urlFor(card?.mainImage.asset._ref)}
+                  alt=""
+                  className="mx-auto"
+                />
                 <h1>{card.title}</h1>
                 <p>{card?.body}</p>
+                <NavLink
+                  className="btn"
+                  // to={`/${contact}`}
+                  aria-label="Request a Call link"
+                >
+                  {card.button}
+                </NavLink>
               </div>
             );
           })}
         </section>
       )}
-      {/* <MyVideo url={aboutVid} /> */}
+
+      {/* Get To Know Us*/}
+      <section className="mx-auto my-10">
+        <h1>{home.homeAbout.title}</h1>
+        <p>{home.homeAbout?.body}</p>
+        <MyVideo url={aboutVid} />
+      </section>
+
+      {/* Espanol Language */}
+      <section className="mx-auto my-10 w-2/3 bg-white">
+        <h1>{home.language.title}</h1>
+        <p>{home.language.body}</p>
+      </section>
     </div>
   );
 };

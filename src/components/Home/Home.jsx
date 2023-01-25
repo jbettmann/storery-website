eb35dc224dc50fdc8b77465bfa21a8f2794552b0
jbettmann <jordan.bettmann@gmail.com>
2023-01-25 13:28:56 -0700

    completing layout of Homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
-
+import { NavLink } from "react-router-dom";
 import sanityClient from "../../client";
 import { MyVideo } from "../MyVideo/MyVideo";
 
 import "./Home.scss";
 
-export const Home = ({ setLogo }) => {
+export const Home = ({ setLogo, contact }) => {
   const [home, setHome] = useState(null);
+  const [cards, setCards] = useState(null);
   const [aboutVid, setAboutVid] = useState(null);
 
+  const setCardArry = (cards) => {
+    let [...newCards] = Object.values(cards);
+    setCards(newCards.filter((doc, i) => typeof doc === "object"));
+    console.log("cards loaded");
+  };
   useEffect(() => {
     sanityClient
       .fetch(
@@ -27,22 +33,38 @@ export const Home = ({ setLogo }) => {
         setHome(newData);
         setAboutVid(newData.homeAbout.videoUrl);
         setLogo(newData.logo);
+        setCardArry(newData.cards);
       })
       .catch(console.error);
   }, [home?.homeAbout.videoUrl]);
-
   if (!home) return <div>Loading...</div>;
   return (
-    <div className="w-200">
-      <h1 className="text-5xl underline decoration-double decoration-4 underline-offset-8 decoration-red-500 ">
-        This is the h1 Title ill be styling
-      </h1>
-      <p className="text-base leading-normal truncate hover:whitespace-normal ">
-        This is the paragraph text ill be styling there are alot of words more
-        then I like to have things there This is the paragraph text ill be
-        styling there are alot of words more then I like to have things there
-      </p>
-      <MyVideo url={aboutVid} />
+    <div>
+      {/* Hero */}
+      <header>
+        <img src="" alt="" />
+        <h1></h1>
+        <h5></h5>
+        <NavLink to={`/${contact}`} aria-label="Request a Call link">
+          Request A Call
+        </NavLink>
+      </header>
+
+      {/* Cards */}
+      {cards && (
+        <section>
+          {cards.map((card, i) => {
+            return (
+              <div key={i}>
+                <img src={card?.image} alt="" />
+                <h1>{card.title}</h1>
+                <p>{card?.body}</p>
+              </div>
+            );
+          })}
+        </section>
+      )}
+      {/* <MyVideo url={aboutVid} /> */}
     </div>
   );
 };

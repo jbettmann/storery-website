827db558b6f62fec7073d4b80cf22d7d2b3aa5dd
jbettmann <jordan.bettmann@gmail.com>
2023-02-10 15:12:23 -0700

    styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
+import { SocialIcon } from "react-social-icons";
+import sanityClient from "../../client";
+
+import zillow from "../../assets/icons/zillow-icon.svg";
 
 export const Contact = () => {
   const [state, handleSubmit] = useForm("xjvdyypp");
@@ -8,15 +12,24 @@ export const Contact = () => {
   }
 
   return (
-    <div
-      className="  h-[100vh] w-full flex justify-center items-center"
-      id="contact"
+    <form
+      className="bg-white h-[100%] w-full border-y-[2rem] border-slate-100 "
       onSubmit={handleSubmit}
+      id="contact"
     >
-      <form className="bg-white h-auto w-[300px]  md:w-[400px] rounded-xl shadow-xl p-3 ">
-        <div className=" text-center text-3xl mt-10">Send a Message</div>
+      <div className="shadow-md w-full p-3">
+        <div className=" border-none text-center text-3xl mt-10">
+          Send a Message
+        </div>
         <div className=" w-full flex justify-center flex-col items-center">
           <div>
+            <input
+              className="outline-none mb-2 rounded-md backdrop-blur-xl bg-white/20 shadow-md px-3 py-3 w-3/4 mb-[-50px]"
+              placeholder="Full Name"
+              name="name"
+              required
+            />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
             <input
               className=" outline-none mt-16 mb-2 rounded-md backdrop-blur-xl bg-white/20 shadow-md px-3 py-3 w-3/4"
               placeholder="your email"
@@ -28,13 +41,6 @@ export const Contact = () => {
               field="email"
               errors={state.errors}
             />
-            <input
-              className=" outline-none mb-2 rounded-md backdrop-blur-xl bg-white/20 shadow-md px-3 py-3 w-3/4"
-              placeholder="Full Name"
-              name="name"
-              required
-            />
-            <ValidationError prefix="Name" field="name" errors={state.errors} />
           </div>
           <div className=" w-3/4">
             <input
@@ -50,7 +56,7 @@ export const Contact = () => {
             />
             <textarea
               className=" outline-none area rounded-md backdrop-blur-xl bg-white/20 shadow-md px-3 py-3 w-full h-40 md:h-52"
-              placeholder="your message"
+              placeholder="Message"
               name="message"
               required
             />
@@ -61,15 +67,12 @@ export const Contact = () => {
             />
           </div>
           <div className=" w-3/4 flex justify-end mt-4">
-            <button
-              className=" bg-blue-600 w-max px-4 py-2 rounded-md underbar flex items-center flex-row gap-4 hover:shadow-xl"
-              disabled={state.submitting}
-            >
+            <button className="btn" disabled={state.submitting}>
               <h2 className=" text-sm font-medium text-white">Get in touch</h2>
             </button>
           </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };

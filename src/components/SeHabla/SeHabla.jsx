18760f5b204fdcda227f634442e054af901eaf1d
jbettmann <jordan.bettmann@gmail.com>
2023-02-23 15:14:54 -0700

    added styling

--- src/components/SeHabla/SeHabla.jsx
+++ src/components/SeHabla/SeHabla.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SeHabla = ({ seHabla }) => {
   return (
     seHabla && (
-      <section className="mx-auto p-6 text-center w-fit bg-white">
+      <section className="bg-storeyGreen-100/10 rounded-lg shadow-md border border-storeyGreen-100 mx-6 lg:mx-auto p-6 text-center w-fit bg-white">
         <h1>{seHabla.language.title}</h1>
         <p>{seHabla.language.body}</p>
       </section>

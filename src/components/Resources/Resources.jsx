b2fc39f52673afa5a01fdadf332dd1918ef4fbe1
jbettmann <jordan.bettmann@gmail.com>
2023-02-23 15:14:47 -0700

    Created Resource page

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getResources } from "../../Functions/Functions";
 
 export const Resources = () => {
+  const [resources, setResources] = useState(null);
+  useEffect(() => {
+    getResources(setResources);
+  }, []);
+  console.log(resources);
   return <div>Resources</div>;
 };

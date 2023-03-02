f7e461f1d31286640e825c0735f72012008516b7
jbettmann <jordan.bettmann@gmail.com>
2023-03-01 17:01:41 -0700

    updated Recourse tab to make two calls

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { getResources } from "../../Functions/Functions";
+
+import { getBlog, getFAQ } from "../../Functions/Functions";
+import { Blog } from "../Blog/Blog";
+import { FAQ } from "../FAQs/FAQs";
 
 export const Resources = () => {
-  const [resources, setResources] = useState(null);
+  const [blogs, setBlog] = useState(null || []);
+  const [faqs, setFAQ] = useState([]);
+
   useEffect(() => {
-    getResources(setResources);
-  }, []);
-  console.log(resources);
-  return <div>Resources</div>;
+    getBlog(setBlog);
+    getFAQ(setFAQ);
+  }, [blogs.length]);
+
+  console.log({ blogs, faqs });
+  if (!blogs && !faqs) return <div>Loading...</div>;
+
+  return (
+    <section>
+      <Blog blogs={blogs} />
+      <div className="bg-white w-screen h-full">
+        {faqs.map((faq, i) => {
+          return <FAQ key={i} faq={faq} />;
+        })}
+      </div>
+    </section>
+  );
 };

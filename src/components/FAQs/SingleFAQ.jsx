4dd84a383e7f2cad5afe3b67f9184a18bc190c0b
jbettmann <jordan.bettmann@gmail.com>
2023-03-03 13:39:02 -0700

    handle function when user clicks on faq

--- src/components/FAQs/SingleFAQ.jsx
+++ src/components/FAQs/SingleFAQ.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 
 export const SingleFAQ = ({ qa }) => {
   const [showAnswer, setShowAnswer] = useState(false);
@@ -7,12 +8,22 @@ export const SingleFAQ = ({ qa }) => {
     setShowAnswer(!showAnswer);
   };
   return (
-    <div onClick={handleClick} className="cursor-pointer">
-      <h4 className="font-bold">{qa.question}</h4>
-      {showAnswer && (
-        <div>
-          <p>{qa.answer}</p>
-        </div>
+    <div
+      onClick={handleClick}
+      className="cursor-pointer p-5 flex justify-between items-center border-b border-slate-700"
+    >
+      <div className="w-5/6 md:w-11/12">
+        <h4 className="font-bold text-storeyGreen-100 m-0">{qa.question}</h4>
+        {showAnswer && (
+          <div>
+            <h5 className="m-0 mt-4 p-0">{qa.answer}</h5>
+          </div>
+        )}
+      </div>
+      {!showAnswer ? (
+        <SlArrowDown size={`1.2rem`} />
+      ) : (
+        <SlArrowUp size={`1.2rem`} />
       )}
     </div>
   );

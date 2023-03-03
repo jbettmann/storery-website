6d3ee0c0aad18d362b26586bea0e20662c7e4246
jbettmann <jordan.bettmann@gmail.com>
2023-03-03 13:39:20 -0700

    updated styling

--- src/components/FAQs/FAQs.jsx
+++ src/components/FAQs/FAQs.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { SingleFAQ } from "./SingleFAQ";
 
-export const FAQ = ({ faq, key }) => {
+export const FAQ = ({ faq, i }) => {
   return (
-    <article key={key}>
-      <h2>{faq.title}</h2>
+    <article key={i} className="h-auto w-5/6 pb-6">
+      <h3 className="py-3 font-bold">{faq.title}</h3>
       {faq.faq?.map((qa) => {
         return <SingleFAQ qa={qa} />;
       })}

8cadaf9cf16e6b73a947e9c6de1b9a517ddb340a
jbettmann <jordan.bettmann@gmail.com>
2023-03-14 15:13:04 -0600

    build out Buying and Selling views

--- src/components/Buy_Sell/BuySell.jsx
+++ src/components/Buy_Sell/BuySell.jsx
@@ -1,38 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { getBuySell, getTestimonials, urlFor } from "../../Functions/Functions";
-import { MortgageCalc } from "../MortgageCalc/MortgageCalc";
+import { BuyTools } from "./BuyTools";
+import { SellQuickTip } from "./SellQuickTip";
 
 export const BuySell = () => {
   const [buySell, setBuySell] = useState({});
   const [active, setActive] = useState(true);
   const [selectedObj, setSelectedObj] = useState(null);
+  const [selBuyTool, setSelBuyTool] = useState("1");
   const [testimonials, setTestimonials] = useState(null);
 
+  // sets display for buy tool
+  const handleToolSel = (e) => {
+    setSelBuyTool(e.target.id);
+  };
+
+  // fetches data and sets states
   useEffect(() => {
     getBuySell(setBuySell);
     getTestimonials(setTestimonials);
-    console.log("Ran");
+    // setSelBuyTool("1");
   }, []);
 
+  // sets state of selectedObj
   useEffect(() => {
     // Set selectedObj based on the active state
     setSelectedObj(active ? buySell.buy : buySell.sell);
-    console.log("2  Ran");
   }, [buySell, active]);
 
-  console.log(buySell, selectedObj);
+  console.log({ buySell, selBuyTool, selectedObj });
   return (
     selectedObj && (
       <section className="">
         <h1 className=" m-4">{buySell.buySellTitle}</h1>
         <article>
-          <img src={urlFor(selectedObj.hero.mainImage.asset._ref)} />
+          <img src={urlFor(selectedObj.hero.mainImage.asset._ref)} alt="" />
           <div>
             <button onClick={() => setActive(!active)}>
               {selectedObj.title}
             </button>
             <h5>{selectedObj.hero.description}</h5>
-            {selectedObj?.whyChecklist.checklist.map((list) => {
+            {selectedObj.whyChecklist.checklist.map((list) => {
               return (
                 <ul>
                   <li>{list.item}</li>
@@ -41,6 +49,28 @@ export const BuySell = () => {
             })}
           </div>
         </article>
+        {active ? (
+          <article>
+            {/* Buy Tools Selection  */}
+
+            <div>
+              <button className="btn" id="1" onClick={handleToolSel}>
+                {selectedObj.mortgageCalculators?.title}
+              </button>
+              <button className="btn" id="2" onClick={handleToolSel}>
+                {selectedObj.lenders?.title}
+              </button>
+              <button className="btn" id="3" onClick={handleToolSel}>
+                {selectedObj.schoolDistricts?.title}
+              </button>
+            </div>
+
+            <BuyTools selBuyTool={selBuyTool} selectedObj={selectedObj} />
+          </article>
+        ) : (
+          <SellQuickTip selectedObj={selectedObj} />
+        )}
+
         {/* <MortgageCalc /> */}
       </section>
     )

d1d24a12fe90f905cd3b5264539d7b24298b159e
jbettmann <jordan.bettmann@gmail.com>
2023-03-20 12:56:05 -0600

    Added BuyTool options

--- src/components/Buy_Sell/BuyTools.jsx
+++ src/components/Buy_Sell/BuyTools.jsx
@@ -4,22 +4,24 @@ import { BuyToolCard } from "../Card/BuyToolCard";
 
 export const BuyTools = ({ selBuyTool, selectedObj }) => {
   return (
-    <div>
-      {selBuyTool === "1" ? (
-        <MortgageCalc />
-      ) : selBuyTool === "2" ? (
-        <div className="flex">
-          {selectedObj.lenders.lendersList.map((lender) => {
-            return <BuyToolCard card={lender} />;
-          })}
-        </div>
-      ) : (
-        <div className="flex">
-          {selectedObj.schoolDistricts.districtsList.map((district) => {
-            return <BuyToolCard card={district} />;
-          })}
-        </div>
-      )}
-    </div>
+    selectedObj && (
+      <div className="flex justify-center my-10">
+        {selBuyTool === "1" ? (
+          <MortgageCalc />
+        ) : selBuyTool === "2" ? (
+          <div className="flex flex-col items-center lg:flex-row gap-10 mx-9 justify-center">
+            {selectedObj.lenders?.lendersList.map((lender) => {
+              return <BuyToolCard card={lender} />;
+            })}
+          </div>
+        ) : (
+          <div className="flex flex-col items-center lg:flex-row gap-10 mx-9 justify-center">
+            {selectedObj.schoolDistricts?.districtsList.map((district) => {
+              return <BuyToolCard card={district} />;
+            })}
+          </div>
+        )}
+      </div>
+    )
   );
 };

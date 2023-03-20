f9aa025d2353a116219e7ebb982335374361aee4
jbettmann <jordan.bettmann@gmail.com>
2023-03-20 12:56:39 -0600

    reuseable buytool card

--- src/components/Card/BuyToolCard.jsx
+++ src/components/Card/BuyToolCard.jsx
@@ -2,39 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { urlFor } from "../../Functions/Functions";
 
-export const BuyToolCard = ({ card, style }) => {
+export const BuyToolCard = ({ card }) => {
   return (
     <a
       href={card.url}
       aria-label={`Go to ${card.name} website`}
       target="_blank"
-      className={`card`}
+      className={`card px-8 max-w-sm`}
       rel="noreferrer"
     >
-      <div className="flex-auto w-full h-full overflow-hidden flex justify-center items-center ">
+      {/* Image or Logo */}
+      <div className="flex justify-center flex-auto w-full h-52 mb-6 overflow-hidden ">
         <img
           src={urlFor(card.mainImage?.asset._ref)}
           alt={card.slug?.current}
-          className={`mx-auto ${style ? style.img : " w-52 "}`}
+          className={`mx-auto w-full h-full object-cover`}
         />
       </div>
 
+      {/* Text/Info container */}
       <div
-        className={`flex flex-col flex-initial sm:items-start lg:justify-between justify-between lg:items-center items-center p-3 lg:p-0 ${
-          style ? style.body : "h-full"
-        }`}
+        className={`flex flex-col flex-initial items-start justify-center gap-6 p-3 lg:p-0 h-10/12 w-full text-left`}
       >
         <div>
           {card.company ? (
-            <h3 className="my-4 text-base md:text-xl font-medium">
+            <h3 className="text-base md:text-xl font-bold text-storeyGreen-100 ">
               {card.company}
             </h3>
           ) : null}
-          <h3>{card.name}</h3>
+          <h4 className="font-bold">{card.name}</h4>
         </div>
         {card.company ? (
-          <div>
-            <p>{card.position}</p>
+          <div className="flex flex-col justify-start">
+            <p className="m-0 p-0">{card.bio}</p>
             <a
               className=""
               aria-label={`Call ${card.name} at ${card.phone}`}
@@ -52,11 +52,13 @@ export const BuyToolCard = ({ card, style }) => {
           </div>
         ) : (
           <a
-            className="btn bg-transparent text-storeyGreen-100 border border-storeyGreen-100"
+            className="bg-transparent text-storeyGreen-100 "
             href={card.url}
-            aria-label="Read more..."
+            target="_blank"
+            aria-label="Learn more..."
+            rel="noreferrer"
           >
-            {card.button ? card.button : "Read more..."}
+            {card.button ? card.button : "Learn more..."}
           </a>
         )}
       </div>

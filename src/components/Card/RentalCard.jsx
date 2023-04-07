fc421569763a2e6ab690065c48da787ebfaa418e
jbettmann <jordan.bettmann@gmail.com>
2023-04-07 13:14:05 -0600

    removed unused imports

--- src/components/Card/RentalCard.jsx
+++ src/components/Card/RentalCard.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { urlFor } from "../../Functions/Functions";
 
 export const RentalCard = ({ benefit, icon, i }) => {
   return (
-    <div className={`card h-auto px-8 max-w-sm`}>
+    <div
+      className={`card green-card hover:cursor-auto h-56 360:h-48 lg:h-56 xl:h-48 px-6 py-8 max-w-sm`}
+    >
       {/* Icon and Benefit Title*/}
-      <div className="flex justify-center flex-auto w-full  mb-6 overflow-hidden ">
+      <div className="flex justify-center items-center flex-auto w-full  overflow-hidden mb-3 ">
         <div className="p-3 benefit-icon">{icon}</div>
 
         <h4 className="font-bold">{benefit.title}</h4>
       </div>
 
       {/* Text/Info container */}
-
-      <p className="m-0 p-0">{benefit.description}</p>
+      <p className="m-0 p-0 ">{benefit.description}</p>
     </div>
   );
 };

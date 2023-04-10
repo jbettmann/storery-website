acf198d88ff9290cb4eceb7902da3f1cc1abd5e9
jbettmann <jordan.bettmann@gmail.com>
2023-04-10 13:56:18 -0600

    added remodel componets

--- src/components/Remodel/Remodel.jsx
+++ src/components/Remodel/Remodel.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { getRemodel, urlFor } from "../../Functions/Functions";
+import BlockContect from "@sanity/block-content-to-react";
+import { SellQuickTip } from "../Buy_Sell/SellQuickTip";
 
-export const Remodel = () => {
+export const Remodel = ({ contact }) => {
+  const [remodel, setRemodel] = useState(null);
+
+  useEffect(() => {
+    getRemodel(setRemodel);
+  }, []);
+  console.log(remodel);
   return (
-    <div className="flex w-full min-h-screen justify-center items-center">
-      <h1>🚧 🛠️ Being Remodeled 🛠️ 🚧</h1>
-    </div>
+    remodel && (
+      <div>
+        {/* Hero */}
+        <section className="my-8 relative text-white">
+          <article className="h-96 lg:h-[30rem] ">
+            <div className="w-full h-[30rem] overflow-hidden ">
+              <img
+                src={urlFor(remodel.hero?.mainImage.asset._ref)}
+                alt="Beautiful sunny brick home surrounded by green trees that Storey owns"
+                className="w-full h-full object-cover brightness-50"
+              />
+            </div>
+            <div className="absolute top-0 left-0 w-full h-full px-6 flex flex-col text-center justify-center items-center flex-auto">
+              <h1>{remodel.hero.webpageTitle}</h1>
+              <NavLink
+                className="btn mt-8"
+                to={`/${contact}`}
+                aria-label={remodel.hero.buttonText}
+              >
+                {remodel.hero.buttonText}
+              </NavLink>
+            </div>
+          </article>
+        </section>
+        <SellQuickTip selectedObj={remodel.remodelPlan} />
+      </div>
+    )
   );
 };

e36f3377295edee6d6e6b95ac83f7dd4cf0a31c5
jbettmann <jordan.bettmann@gmail.com>
2023-04-20 14:11:35 -0600

    added max screen size

--- src/components/InvestmentProp/InvestmentProp.jsx
+++ src/components/InvestmentProp/InvestmentProp.jsx
@@ -97,74 +97,76 @@ export const InvestmentProp = ({
             {investment.webpageTitle}
           </h1>
         </div>
-        {/* Hero Buy / Sell */}
-        <article className="flex flex-col items-center bg-white w-full p-6 md:p-14 gap-10 lg:flex-row lg:items-start">
-          <div className="w-full lg:w-3/5 h-56 sm:h-96">
-            <img
-              className="w-full h-full object-cover rounded-2xl"
-              src={urlFor(selectedObj.hero.mainImage.asset._ref)}
-              alt=""
-            />
-          </div>
-
-          {/* Toggle View */}
-          <div className="w-full lg:w-3/5 ">
-            {/* Toggle Buttons */}
-            <div className="mx-auto lg:mx-0 flex items-center w-full xs:w-2/3 sm:w-1/2 h-16 relative mb-6 justify-around bg-gray-300 rounded-md">
-              <div
-                className={`absolute top-0 left-0 w-1/2 h-full btn transition-transform duration-300 ease-in-out  ${
-                  viewOption === "rental"
-                    ? "transform translate-x-0 "
-                    : "transform translate-x-full "
-                }`}
-              ></div>
-              <button
-                className={`toggle-btn ${
-                  viewOption === "rental"
-                    ? " text-white font-bold"
-                    : " text-black"
-                }`}
-                onClick={() => handleOptionClick("rental")}
-              >
-                <h4>{investment.rental.title}</h4>
-              </button>
-              <button
-                className={`toggle-btn  ml-4 ${
-                  viewOption === "fixFlip"
-                    ? " text-white font-bold"
-                    : " text-black"
-                }`}
-                onClick={() => handleOptionClick("fixFlip")}
-              >
-                <h4>{investment.fixFlip.title}</h4>
-              </button>
+        {/* Hero Invest */}
+        <article className="bg-white w-full ">
+          <div className="w-full toggle-container flex flex-col items-center mx-auto p-6 md:p-14 gap-10 lg:flex-row lg:items-start">
+            <div className="w-full lg:w-3/5 h-56 sm:h-96">
+              <img
+                className="w-full h-full object-cover rounded-2xl"
+                src={urlFor(selectedObj.hero.mainImage.asset._ref)}
+                alt=""
+              />
             </div>
 
-            {/* Hero Info */}
-            <div>
-              <h3 className="font-bold ">{selectedObj.hero.description}</h3>
+            {/* Toggle View */}
+            <div className="w-full lg:w-3/5 ">
+              {/* Toggle Buttons */}
+              <div className="mx-auto lg:mx-0 flex items-center w-full xs:w-2/3 sm:w-1/2 h-16 relative mb-6 justify-around bg-gray-300 rounded-md">
+                <div
+                  className={`absolute top-0 left-0 w-1/2 h-full btn transition-transform duration-300 ease-in-out  ${
+                    viewOption === "rental"
+                      ? "transform translate-x-0 "
+                      : "transform translate-x-full "
+                  }`}
+                ></div>
+                <button
+                  className={`toggle-btn ${
+                    viewOption === "rental"
+                      ? " text-white font-bold"
+                      : " text-black"
+                  }`}
+                  onClick={() => handleOptionClick("rental")}
+                >
+                  <h4>{investment.rental.title}</h4>
+                </button>
+                <button
+                  className={`toggle-btn  ml-4 ${
+                    viewOption === "fixFlip"
+                      ? " text-white font-bold"
+                      : " text-black"
+                  }`}
+                  onClick={() => handleOptionClick("fixFlip")}
+                >
+                  <h4>{investment.fixFlip.title}</h4>
+                </button>
+              </div>
 
-              {selectedObj.rentalHelpList ? (
-                <ul>
-                  {selectedObj.rentalHelpList.list.map((list) => {
-                    return (
-                      <li className="flex items-center mt-6">
-                        <div className="mr-3">
-                          <span className="inline-block h-2 w-2 rounded-full bg-storeyGreen-100 "></span>
-                        </div>
-                        <span className="text-black">{list.item}</span>
-                      </li>
-                    );
-                  })}
-                </ul>
-              ) : (
-                <BlockContent
-                  blocks={selectedObj.hero.subDescription}
-                  projectId="k4xvtsjp"
-                  dataset="production"
-                  className="prose-p:m-0 prose-p:pl-0"
-                />
-              )}
+              {/* Hero Info */}
+              <div>
+                <h3 className="font-bold ">{selectedObj.hero.description}</h3>
+
+                {selectedObj.rentalHelpList ? (
+                  <ul>
+                    {selectedObj.rentalHelpList.list.map((list) => {
+                      return (
+                        <li className="flex items-center mt-6">
+                          <div className="mr-3">
+                            <span className="inline-block h-2 w-2 rounded-full bg-storeyGreen-100 "></span>
+                          </div>
+                          <span className="text-black">{list.item}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                ) : (
+                  <BlockContent
+                    blocks={selectedObj.hero.subDescription}
+                    projectId="k4xvtsjp"
+                    dataset="production"
+                    className="prose-p:m-0 prose-p:pl-0"
+                  />
+                )}
+              </div>
             </div>
           </div>
         </article>

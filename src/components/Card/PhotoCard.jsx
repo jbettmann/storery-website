46e1c322859f3d3a32dd4ed6a6a76366cb7ebac0
jbettmann <jordan.bettmann@gmail.com>
2023-09-15 12:46:04 -0600

    added place holder

--- src/components/Card/PhotoCard.jsx
+++ src/components/Card/PhotoCard.jsx
@@ -15,7 +15,7 @@ export const PhotoCard = ({ item, beforeAfter, isActive }) => {
   }, [isActive]);
   return (
     <div className="flex-auto w-1/2 h-full overflow-hidden flex justify-center items-center p-2 sm:p-0 bg-white rounded-2xl">
-      <div className="relative w-full md:w-5/6 lg:w-2/3 xl:w-1/2 sm:py-8 sm:px-6">
+      <div className="relative remodel w-full md:w-5/6 lg:w-2/3 xl:w-1/2 sm:py-8 sm:px-6">
         {/* Before */}
         <img
           src={urlFor(item.beforeImage.asset._ref)}
